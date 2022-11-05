43fbdf6f5198c83cf728b265d40baada824b5ca8
Haroon Akram <haroonakram080@gmail.com>
2022-11-05 18:22:52 -0400

    Added a function to access the private api key from a file that is not accessible to others.

--- weather.py
+++ weather.py
@@ -1,15 +1,19 @@
 import requests
 import geocoder
+import configparser
 
 # Returns a json of the weather data such as temperature, sky, humidity, wind, etc.
 def get_json_weather_data_From_lat_lon(lat, lon, api_key):
     api_url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}"
     return requests.get(url=api_url).json()
 
-
+def get_api_key():
+    config = configparser.ConfigParser()
+    config.read('private_data.ini')
+    return config["OPENWEATHER"]["api_key"]
 
 # Main starts here
 g = geocoder.ip('me')
-#print(get_json_weather_data_From_lat_lon(g.latlng[0], g.latlng[1], api_key)['weather'])
+print(get_json_weather_data_From_lat_lon(g.latlng[0], g.latlng[1], get_api_key()))
 
 #print(g.latlng)
